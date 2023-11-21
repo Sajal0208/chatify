03a682a8016515218df1e11a9f453c4aefa86c1f
Sajal0208 <sajaldewangan@gmail.com>
2023-11-21 21:55:22 +0530

    Did something

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
 import App from './App';
-import { Provider } from 'react-redux';
-import store from './store/store';
 import { ChakraProvider } from "@chakra-ui/react";
+import store from './store/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
